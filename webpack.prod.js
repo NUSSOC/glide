9e346853e229c072e033550ed2aca413b2ff5dad
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-02 17:17:22 +0800

    chore(webpack): add br, gz compression for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,5 @@
+const zlib = require('zlib');
+const CompressionPlugin = require('compression-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common');
@@ -5,4 +7,20 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  plugins: [
+    new CompressionPlugin({
+      test: /\.(js|css|html|svg|data|wasm)$/,
+      algorithm: 'gzip',
+    }),
+    new CompressionPlugin({
+      algorithm: 'brotliCompress',
+      test: /\.(js|css|html|svg|data|wasm)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+    }),
+  ],
 });
