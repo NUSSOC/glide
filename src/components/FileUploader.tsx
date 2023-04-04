b6b5ab38ceb52b26cdc001ff029f982b13d63268
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-04 19:39:34 +0800

    feat(fileuploader): allow plaintexts

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -35,7 +35,7 @@ const FileUploader = (props: FileUploaderProps): JSX.Element => {
     >
       {props.children}
       <input
-        accept="text/csv, text/x-python-script, text/x-python, .py, .csv"
+        accept="text/csv, text/x-python-script, text/x-python, .py, .csv, text/plain"
         className="absolute bottom-0 left-0 right-0 top-0 cursor-pointer opacity-0"
         onChange={handleUpload}
         type="file"
