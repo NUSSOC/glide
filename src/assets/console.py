63eed532762bd531f15ae16471daec21e2eb8327
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2023-04-06 22:25:32 +0800

    feat(interpreter): reset globals every time code is run

--- src/assets/console.py
+++ src/assets/console.py
@@ -1,16 +1,17 @@
-import sys
 from pyodide.ffi import to_js
 from pyodide.console import PyodideConsole, repr_shorten, BANNER
 import __main__
 import builtins
 
-pyconsole = PyodideConsole(__main__.__dict__)
-
 async def await_fut(fut):
-  res = await fut
-  if res is not None:
-    builtins._ = res
-  return to_js([res], depth=1)
+  result = await fut
+  if result is not None:
+    builtins._ = result
+  return to_js([result], depth=1)
 
-def clear_console():
-  pyconsole.buffer = []
+def create_console(new_globals = __main__.__dict__):
+  return PyodideConsole(new_globals)
+
+def clear_console(console):
+  if console is not None:
+    console.buffer = []
